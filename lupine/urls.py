3af5fde096b672f446780f62ab159b462755bb64
harshit <harshit@cclub.com>
2015-02-17 21:41:24 +0530

    Initialized Django app named --lupine--

---
+++ lupine/urls.py
@@ -0,0 +1,13 @@
+__author__ = 'harshit'
+from django.conf.urls import include, patterns, url
+from lupine import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'warbird.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.index, name="index"),
+    url(r'^callback', views.callback, name="callback"),
+    url(r'^clean', views.clean, name="clean"),
+    url(r'^webhooks', views.hooks, name="hooks")
+)
---
+++ lupine/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required, permission_required
+from warbird.settings import Github_client_id, Github_client_secret
+import requests as req
+from uritemplate import expand
+from github import Github, GithubException
+import json
+
+# Create your views here.
+@login_required
+def home(request):
+    return render(request, 'Index.html')
+
+@login_required
+def callback(request):
+    url = expand("https://github.com/login/oauth/access_token{?client_id,client_secret,code}",
+                 client_id = Github_client_id, client_secret = Github_client_secret, code = request.GET["code"])
+    r = req.post(url, headers={"Accept": "application/json"}).json()
+    request.session["github_token"] = r["access_token"]
+    if r["scope"] == "":
+        request.session["github_scopes"] = []
+    else:
+        request.session["github_scopes"] = r["scope"].split(",")
+    return redirect("lupine.views.index")
+
+def index(request):
+    g = buildPyGithub(request)
+    if g is None:
+        return render(request,"Index.html",{"github_authorize_url": "https://github.com/login/oauth/authorize?client_id=f14052faddcf7e3fe42d",})
+    else:
+        try:
+            return render(request,"repolist.html",{"github_authorize_url": "https://github.com/login/oauth/authorize?client_id=f14052faddcf7e3fe42d&scope=repo,user:email,read:repo_hook","repos": g.get_user().get_repos(),"github_scopes": request.session["github_scopes"],})
+        except GithubException:
+            return render(request,"Index.html",{"github_authorize_url": "https://github.com/login/oauth/authorize?client_id=f14052faddcf7e3fe42d",})
+
+def buildPyGithub(request):
+    token = request.session.get("github_token")
+    if token is None:
+        return None
+    else:
+        print token
+        return Github(login_or_token=token, client_id=Github_client_id, client_secret=Github_client_secret)
+
+def clean(request):
+    request.session.clear()
+    return redirect(index(request))
+
+def hooks(request):
+    if request.method == 'POST':
+        reponame = request.POST['repo_name']
+        g = buildPyGithub(request)
+        url =expand("https://localhost:8000/webhooks/payload")
+        print reponame
